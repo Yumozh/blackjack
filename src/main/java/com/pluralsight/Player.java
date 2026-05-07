f1d153594f269b2ba2895ab82c15858a95168672
Yumozh <juliiamozhchil@gmail.com>
2026-05-07 14:51:24 -0500

    Add Player

---
+++ src/main/java/com/pluralsight/Player.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class Player {
+    private String name;
+    private Hand hand;
+    private boolean isBusted;  //A flag to track if the player has gone over 21. This makes it easier to determine the winner at the end.
+    private boolean isStaying;
+
+    public Player(String name) {
+        this.name = name;
+    }
+}
